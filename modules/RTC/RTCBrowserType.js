93d490b0d78a358c72693f501fffa1e5aa6ab7b8
alex <alexkovj27@gmail.com>
2017-10-01 20:23:55 +0500

    disable p2p for react native

--- modules/RTC/RTCBrowserType.js
+++ modules/RTC/RTCBrowserType.js
@@ -129,7 +129,7 @@ const RTCBrowserType = {
      * otherwise.
      */
     isP2PSupported() {
-        return !RTCBrowserType.isEdge();
+        return !RTCBrowserType.isEdge() && !RTCBrowserType.isReactNative();
     },
 
     /**
